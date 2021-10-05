b1a97676652dd4c58e5e66f8866d5ff785a7e6c2
Ezeial <eziogiraldi.dev@gmail.com>
2021-10-05 14:30:54 +0200

    fix_map

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -5,8 +5,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *))
 	t_list	*first;
 	t_list	*new;
 
-	new = ft_lstnew(f(lst->content));
-	first = new;
+	first = NULL;
 	while (lst)
 	{
 		new = ft_lstnew(f(lst->content));
